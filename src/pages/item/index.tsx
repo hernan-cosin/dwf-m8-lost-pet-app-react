89ad6f0295ddb9778b8717cc1f4f477f2c6421c0
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-04-18 11:27:08 -0300

    remove importe ImageSlider

--- src/pages/item/index.tsx
+++ src/pages/item/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react"
 import { useParams } from "react-router-dom";
-import {ImageSlider} from "components/carousel"
+// import {ImageSlider} from "components/carousel"
 import {useItemResult} from "hooks/hooks"
 import css from "./item.css"
 
