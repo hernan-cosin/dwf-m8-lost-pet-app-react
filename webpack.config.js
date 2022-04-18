c8d707558ac706b8d1a9c4b53e57e38f07d12f01
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-04-18 11:47:49 -0300

    added performance item to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -47,4 +47,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+}
 };
